6620b0b886d4b7cdc7ced9e125eafcd7628d2f28
joaocampos96 <joao.campos@pt.softinsa.com>
2019-03-08 23:06:23 +0000

    tablayout a funcionar sendo chamado dentro de um fragment.

--- app/src/main/java/com/example/projetofinal/FragmentOne.kt
+++ app/src/main/java/com/example/projetofinal/FragmentOne.kt
@@ -3,10 +3,13 @@ package com.example.projetofinal
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_fragment_one.*
+import kotlinx.android.synthetic.main.second_activity.*
+import kotlinx.android.synthetic.main.second_activity.view.*
 
 
 class FragmentOne : Fragment() {
@@ -16,11 +19,16 @@ class FragmentOne : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_one, container, false)
+        val view =  inflater.inflate(R.layout.second_activity, container, false)
+        val adapter = ViewPagerAdapter(childFragmentManager)
+        adapter.addFragment(FragmentTeste1(),"One")
+        adapter.addFragment(FragmentTeste2(),"two")
+        view.viewpager.adapter = adapter
+        view.tabs.setupWithViewPager(view.viewpager)
+
+        return view
+
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        fragment_one.text="Fragment One"
-    }
 
 }
