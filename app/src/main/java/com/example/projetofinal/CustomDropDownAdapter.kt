8cca370cfa30f07ed178636321783a1e477595d4
joaocampos96 <joao.campos@pt.softinsa.com>
2019-03-10 23:48:13 +0000

    update fim de semana

---
+++ app/src/main/java/com/example/projetofinal/CustomDropDownAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.projetofinal
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+
+
+class CustomDropDownAdapter(val context: Context, var listItemsTxt: Array<String>) : BaseAdapter() {
+
+
+    val mInflater: LayoutInflater = LayoutInflater.from(context)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View
+        val vh: ItemRowHolder
+        if (convertView == null) {
+            view = mInflater.inflate(R.layout.view_drop_down_menu, parent, false)
+            vh = ItemRowHolder(view)
+            view?.tag = vh
+        } else {
+            view = convertView
+            vh = view.tag as ItemRowHolder
+        }
+
+        // setting adapter item height programatically.
+
+        val params = view.layoutParams
+        params.height = 100
+        view.layoutParams = params
+
+        vh.label.text = listItemsTxt.get(position)
+        return view
+    }
+
+    override fun getItem(position: Int): Any? {
+
+        return null
+
+    }
+
+    override fun getItemId(position: Int): Long {
+
+        return 0
+
+    }
+
+    override fun getCount(): Int {
+        return listItemsTxt.size
+    }
+
+    private class ItemRowHolder(row: View?) {
+
+        val label: TextView
+
+        init {
+            this.label = row?.findViewById(R.id.txtDropDownLabel) as TextView
+        }
+    }
+}
--- app/src/main/java/com/example/projetofinal/FragmentTwo.kt
+++ app/src/main/java/com/example/projetofinal/FragmentTwo.kt
@@ -1,23 +1,18 @@
 package com.example.projetofinal
 
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_fragment_one.*
-import kotlinx.android.synthetic.main.pesquisar.view.*
-import android.app.Activity
-import android.content.Context
-import android.graphics.Color
-import android.view.KeyEvent
 import android.view.inputmethod.InputMethodManager
-import android.widget.ArrayAdapter
-import android.widget.Spinner
 import android.widget.AdapterView
-import android.widget.TextView
-import kotlinx.android.synthetic.main.pesquisar.*
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.pesquisar.view.*
 
 
 class FragmentTwo : Fragment() {
@@ -32,7 +27,7 @@ class FragmentTwo : Fragment() {
         view.constraintclick.setOnFocusChangeListener { v, hasFocus -> view.constraintclick.hideKeyboard() }
 
         view.constraintclick.setOnClickListener {
-           // view.constraintclick.hideKeyboard()
+            // view.constraintclick.hideKeyboard()
         }
         view.searchbar.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
@@ -48,6 +43,30 @@ class FragmentTwo : Fragment() {
         })
 
 
+        val listItemsTxt = arrayOf("Last 5 years", "Last 10 years", "Last 15 years", "Last 20 years")
+
+        var spinnerAdapter: CustomDropDownAdapter = CustomDropDownAdapter(context!!, listItemsTxt)
+        var spinner: Spinner = view.findViewById(R.id.spinner1) as Spinner
+        spinner?.adapter = spinnerAdapter
+        spinner.setSelection(1)
+
+
+        spinner?.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                println("oedodeo")
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                //parent.getChildAt(position).setBackgroundColor(Color.BLACK)
+                //parent.getChildAt(position).setBackgroundColor(0x00000000)
+                println("ee")
+                //((TextView) parent.getChildAt(0)).setTextColor(0x00000000);
+            }
+        })
+
+
+        /*
         //get the spinner from the xml.
         val dropdown = view.spinner1
         //create a list of items for the spinner.
@@ -55,39 +74,33 @@ class FragmentTwo : Fragment() {
         val items = arrayOf("5", "10", "15","20")
         //create an adapter to describe how the items are displayed, adapters are used in several places in android.
         //There are multiple variations of this, but this is the basic variant.
-        val adapter = ArrayAdapter(activity, android.R.layout.simple_spinner_dropdown_item, items)
-//set the spinners adapter to the previously created one.
-        dropdown.setAdapter(adapter)
+        //val adapter = ArrayAdapter(activity, android.R.layout.simple_spinner_dropdown_item, items)
+    //set the spinners adapter to the previously created one.
+        //dropdown.setAdapter(adapter)
         dropdown.setSelection(1)
 
-
+    */
 
 
         view.advancedoptions.setOnClickListener {
-            if(view.linear_visibility.visibility==View.INVISIBLE){
+            if (view.linear_visibility.visibility == View.INVISIBLE) {
                 view.advancedoptions.setTextColor(Color.GRAY)
-                view.linear_visibility.visibility=View.VISIBLE
-            }
-            else{
+                view.linear_visibility.visibility = View.VISIBLE
+            } else {
                 view.advancedoptions.setTextColor(Color.WHITE)
-                view.linear_visibility.visibility=View.INVISIBLE
+                view.linear_visibility.visibility = View.INVISIBLE
             }
         }
-
 
 
         return view
     }
 
 
-
     fun View.hideKeyboard() {
         val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(windowToken, 0)
     }
-
-
-
 
 
 }
