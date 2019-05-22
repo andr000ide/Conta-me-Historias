77f68bf3d97156139a6b7cbb9b503608051d7b94
Simão Samouco <simao.samouco@pt.softinsa.com>
2019-05-22 15:23:49 +0100

    credits and agradecimentos onClick nas imagens

--- app/src/main/java/com/example/projetofinal/Fragments/FragmentAgradecimentos.kt
+++ app/src/main/java/com/example/projetofinal/Fragments/FragmentAgradecimentos.kt
@@ -1,18 +1,22 @@
 package com.example.projetofinal.Fragments
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 
 import com.example.projetofinal.R
 import kotlinx.android.synthetic.main.fragment_credits.*
+import kotlinx.android.synthetic.main.fragment_fragment_agradecimentos.*
+import kotlinx.android.synthetic.main.fragment_fragment_agradecimentos.view.*
 
 
-class FragmentAgradecimentos : Fragment() {
+class FragmentAgradecimentos() : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -22,10 +26,36 @@ class FragmentAgradecimentos : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        
+        val view = inflater.inflate(R.layout.fragment_fragment_agradecimentos, container, false)
+
+        view.ola1.setOnClickListener {
+            var link: String = "http://portal2.ipt.pt/"
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+
+            ContextCompat.startActivity(activity!!.applicationContext, intent, null)
+        }
+        view.ola2.setOnClickListener {
+            var link: String = "https://sigarra.up.pt/fcup/pt/web_page.inicial"
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+
+            ContextCompat.startActivity(activity!!.applicationContext, intent, null)
+        }
+        view.ola3.setOnClickListener {
+            var link: String = "https://www.kyoto-u.ac.jp/en/"
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+
+            ContextCompat.startActivity(activity!!.applicationContext, intent, null)
+        }
+        view.ola4.setOnClickListener {
+            var link: String = "https://www.inesctec.pt/en/centres/liaad"
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+
+            ContextCompat.startActivity(activity!!.applicationContext, intent, null)
+        }
 
 
-        return inflater.inflate(R.layout.fragment_fragment_agradecimentos, container, false)
+
+        return view
     }
 
 
--- app/src/main/java/com/example/projetofinal/Fragments/fragment_credits.kt
+++ app/src/main/java/com/example/projetofinal/Fragments/fragment_credits.kt
@@ -1,5 +1,7 @@
 package com.example.projetofinal.Fragments
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -12,6 +14,8 @@ import androidx.core.content.ContextCompat
 
 import com.example.projetofinal.R
 import kotlinx.android.synthetic.main.fragment_credits.*
+import kotlinx.android.synthetic.main.fragment_credits.view.*
+import org.jetbrains.anko.toast
 
 
 class fragment_credits : Fragment() {
@@ -26,8 +30,62 @@ class fragment_credits : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_credits, container, false)
 
-        return inflater.inflate(R.layout.fragment_credits, container, false)
+        view.img_github2.setOnClickListener {
+            var link: String = "https://github.com/simaosamouco"
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+
+            ContextCompat.startActivity(activity!!.applicationContext, intent, null)
+        }
+        view.img_github.setOnClickListener {
+            var link: String = "https://github.com/andr000ide"
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+
+            ContextCompat.startActivity(activity!!.applicationContext, intent, null)
+        }
+        view.img_linkedin.setOnClickListener {
+            var link: String = "https://www.linkedin.com/in/jo%C3%A3o-ricardo-campos-869918167/"
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+
+            ContextCompat.startActivity(activity!!.applicationContext, intent, null)
+        }
+        view.img_linkedin2.setOnClickListener {
+            var link: String = "https://www.linkedin.com/in/sim%C3%A3o-samouco-48916915b/"
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+
+            ContextCompat.startActivity(activity!!.applicationContext, intent, null)
+        }
+        view.img_linkedin3.setOnClickListener {
+            var link: String = "https://www.linkedin.com/in/camposricardo/"
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+
+            ContextCompat.startActivity(activity!!.applicationContext, intent, null)
+        }
+        view.img_mail.setOnClickListener(){
+            var mail = "andr1ide@hotmail.com"
+            val clipboard = context!!.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("copy text", mail)
+            clipboard.primaryClip = clip
+            context!!.toast(mail + "\n O email foi copiado.")
+        }
+        view.img_mail2.setOnClickListener(){
+            var mail = "simao.samouco@hotmail.com"
+            val clipboard = context!!.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("copy text", mail)
+            clipboard.primaryClip = clip
+            context!!.toast(mail + "\n O email foi copiado.")
+        }
+        view.img_mail3.setOnClickListener(){
+            var mail = "ricardo.campos@ipt.pt"
+            val clipboard = context!!.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("copy text", mail)
+            clipboard.primaryClip = clip
+            context!!.toast(mail + "\n O email foi copiado.")
+        }
+
+
+        return view
     }
 
 
