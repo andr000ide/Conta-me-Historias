1443c096b9a1e0d3c6d34fcabe4c70039ed9adfb
joaocampos96 <joao.campos@pt.softinsa.com>
2019-07-04 12:46:53 +0100

    feito i think

--- app/src/main/java/com/example/projetofinal/MediaAdapter.kt
+++ app/src/main/java/com/example/projetofinal/MediaAdapter.kt
@@ -2,10 +2,18 @@ package com.example.projetofinal
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.text.Html
+import android.text.SpannableStringBuilder
 import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.URLSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projetofinal.modelclass.Media
 import com.squareup.picasso.Picasso
@@ -26,10 +34,10 @@ class MediaAdapter(val mediaEle : ArrayList<Media>, val context: Context) : Recy
     }
 
     override fun onBindViewHolder(p0: ViewHolderFour, p1: Int) {
-        p0.mediaTitulo.text = mediaEle.get(p1).titulo
-        p0.mediaTexto.text = mediaEle.get(p1).texto
+        setTextViewHTML(p0.mediaTitulo,mediaEle.get(p1).titulo)
+        setTextViewHTML(p0.mediaTexto,mediaEle.get(p1).texto)
         p0.mediaOrganizacao.text = mediaEle.get(p1).organizacao
-        p0.mediaLink.text = mediaEle.get(p1).link
+        //p0.mediaLink.text = mediaEle.get(p1).link
         //p0.mediaImg.image = mediaEle.get(p1).img
         Picasso.get().load(mediaEle.get(p1).img).into(p0.mediaImg)
 
@@ -54,13 +62,37 @@ class MediaAdapter(val mediaEle : ArrayList<Media>, val context: Context) : Recy
             //notifyItemChanged(p1)
         }
     }
+    protected fun setTextViewHTML(text: TextView, html: String) {
+        val sequence = Html.fromHtml(html)
+        val strBuilder = SpannableStringBuilder(sequence)
+        val urls = strBuilder.getSpans(0, sequence.length, URLSpan::class.java)
+        for (span in urls) {
+            makeLinkClickable(strBuilder, span)
+        }
+        text.text = strBuilder
+        text.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    protected fun makeLinkClickable(strBuilder: SpannableStringBuilder, span: URLSpan) {
+        val start = strBuilder.getSpanStart(span)
+        val end = strBuilder.getSpanEnd(span)
+        val flags = strBuilder.getSpanFlags(span)
+        val clickable = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                var intent = Intent(Intent.ACTION_VIEW, Uri.parse(span.url))
+                ContextCompat.startActivity(context!!, intent, null)
+            }
+        }
+        strBuilder.setSpan(clickable, start, end, flags)
+        strBuilder.removeSpan(span)
+    }
 }
 
 class ViewHolderFour (view: View) : RecyclerView.ViewHolder(view) {
     val mediaTitulo = view.titulo
     val mediaOrganizacao = view.organizacao
     val mediaTexto = view.texto
-    val mediaLink = view.link
+    //val mediaLink = view.link
     val sub_item = view.sub_item
     val mediaImg = view.media_img
 
